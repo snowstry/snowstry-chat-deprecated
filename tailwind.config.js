b2f40e02b1ab003b4e31b02c73d2ec7c009ddbfa
rv178 <idliyout@gmail.com>
2022-07-29 21:53:26 +0530

    [update] organize project structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./frontend/components/**/*.{js,ts,jsx,tsx}",
+		"./src/pages/**/*.{js,ts,jsx,tsx}",
+		"./src/frontend/components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		colors: {
