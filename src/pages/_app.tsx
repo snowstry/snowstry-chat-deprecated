2358842fc1e0e834956fac1054891aa6c062e648
rv178 <idliyout@gmail.com>
2022-08-03 17:20:25 +0530

    [feat] loading bar with nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import "@frontend/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-//import Loading from "@frontend/components/LoadingScreen";
+
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import NProgress from "nprogress";
+import "@frontend/styles/nprogress.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+	const router = useRouter();
+	NProgress.configure({ showSpinner: false });
+
+	useEffect(() => {
+		router.events.on("routeChangeStart", () => NProgress.start());
+		router.events.on("routeChangeComplete", () => NProgress.done());
+		router.events.on("routeChangeError", () => NProgress.done());
+	}, []);
+
 	return (
 		<SessionProvider session={session}>
 			<Component {...pageProps} />
